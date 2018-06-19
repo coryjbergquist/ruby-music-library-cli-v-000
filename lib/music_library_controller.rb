61078a4f3598ea317519fd2dc45c4eff4c4a709d
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-06-19 01:33:02 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -120,8 +120,11 @@ class MusicLibraryController
       song_list[x.name] = x.artist.name
     end
     ordered_list = Hash[song_list.sort]
-    puts "Playing #{ordered_list.keys[song_number]} by #{ordered_list.values[song_number]}" if song_number.between?(1, song_count)
+    if song_number.between?(1, song_count)
+      puts "Playing #{ordered_list.keys[song_number]} by #{ordered_list.values[song_number]}"
+    else
 
+    end
   end
 
 end
