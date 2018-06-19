6fa082dd83ef5ebd55e58627eee1732b5e02252b
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-06-19 01:25:21 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -123,8 +123,6 @@ class MusicLibraryController
     if song_number.between?(1, song_count)
       puts "Playing #{ordered_list.keys[song_number]} by #{ordered_list.values[song_number]}"
     end
-    binding.pry
-
   end
 
 end
