f4dd7732224015fa6230ecfbca0492eb60e4aa2b
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-06-19 01:32:48 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -121,7 +121,7 @@ class MusicLibraryController
     end
     ordered_list = Hash[song_list.sort]
     puts "Playing #{ordered_list.keys[song_number]} by #{ordered_list.values[song_number]}" if song_number.between?(1, song_count)
-    
+
   end
 
 end
